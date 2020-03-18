7cfe138e0a29a3a69ca4cb7c11b67230198f632c
Ángel Lozano <angel.lozano@elecnor.es>
2020-03-18 15:38:38 +0100

    EOEPCA-66: create first template for python library

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'EOEPCA-PyLibraryTemplate',         # How you named your package folder (MyLibrary)
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  author = 'EOEPCA',
+  author_email = 'richard.conway@telespazio.com',
+  description = 'Dummy example for UM PyLibrary Template',   # Give a short description about your library
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  url = 'https://github.com/EOEPCA/um-pylibrary-template',   # Provide the link to the github
+
+  packages=setuptools.find_packages(),
+  license='apache-2.0',        # Do NOT change this unless you know what you are doing
+
+
+  keywords = ['Dummy', 'Template', 'User', 'Management'],   # Keywords that define your package best, in order to find it on a search
+  install_requires=[             # your import list, as in "requirements.txt"
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache license 2.0',
+  ],
+  python_requires='>=3.6',
+)
